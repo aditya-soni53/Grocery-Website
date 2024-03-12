efa8a0fb675f7f2941de7f0dfdd4872c0ae66737
aditya <adityasoni53@gmail.com>
2024-03-13 02:52:25 +0530

    completed final commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,86 @@
 import React from "react";
+import { FaFacebookF, FaGoogle, FaInstagram, FaTelegram } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="bg-gradient-to-r from-primary to-secondary pt-12 pb-8 text-white">
+      <div className="container">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+          {/* company details section */}
+          <div data-aos="fade-up" className="space-y-6 ">
+            <h1 className="text-3xl font-bold">Orange Mint</h1>
+            <p className="text-sm max-w-[300px]">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet
+              earum delectus libero. Beatae, illo! Nisi voluptatum laudantium in
+              tempora natus fugit deserunt dolorum, magnam earum?
+            </p>
+          </div>
+          {/* NavLink section */}
+          <div data-aos="fade-up" data-aos-delay="300" className="space-y-6">
+            <h1 className="text-3xl font-bold">Quick Links</h1>
+            <div className="grid grid-cols-2 gap-3">
+              {/* first column links */}
+              <div>
+                <ul className="space-y-2">
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">About</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact</a>
+                  </li>
+                  <li>
+                    <a href="#">Search Fruits</a>
+                  </li>
+                  <li>
+                    <a href="#">Privacy Policy</a>
+                  </li>
+                </ul>
+              </div>
+              {/* second column links */}
+              <div>
+                <ul className="space-y-2">
+                  <li>
+                    <a href="#">Home</a>
+                  </li>
+                  <li>
+                    <a href="#">About</a>
+                  </li>
+                  <li>
+                    <a href="#">Contact</a>
+                  </li>
+                  <li>
+                    <a href="#">Search Fruits</a>
+                  </li>
+                  <li>
+                    <a href="#">Privacy Policy</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          {/* social links section */}
+          <div data-aos="fade-up" data-aos-delay="300" className="space-y-6">
+            <h1 className="text-3xl font-bold">Follow Us</h1>
+            <div>
+              <p>+1 (123) 585-2563</p>
+              <p>Jaipur , Rajasthan</p>
+            </div>
+            <div className="flex items-center gap-3">
+              <FaFacebookF className="text-3xl hover:scale-105 duration-300" />
+              <FaInstagram className="text-3xl hover:scale-105 duration-300" />
+              <FaTelegram className="text-3xl hover:scale-105 duration-300" />
+              <FaGoogle className="text-3xl hover:scale-105 duration-300" />
+            </div>
+          </div>
+        </div>
+        {/* copyright section */}
+        <p className="text-white text-center mt-8 pt-8 border-t-2">Copyright &copy; aditya .All rights reserved.</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/WhereToBuy/WhereToBuy.jsx
+++ src/components/WhereToBuy/WhereToBuy.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import worldmap from "../../assets/world-map.png";
 
 const WhereToBuy = () => {
-  return <div>WhereToBuy</div>;
+  return (
+    <>
+      <div className="container my-36">
+        <div className="grid grid-cols-1 sm:grid-cols-3 gap-8 place-items-center">
+          {/* text-content section  */}
+          <div className="space-y-8">
+            <h1
+              data-aos="fade-up"
+              data-aos-delay="300"
+              className="text-2xl font-bold text-dark font-serif"
+            >
+              Where to buy our product?
+            </h1>
+            <div
+              data-aos="fade-up"
+              data-aos-delay="500"
+              className="flex items-center gap-4"
+            >
+              <input
+                type="text"
+                placeholder="Country"
+                className="input-style w-full lg:w-[120px]"
+              />
+              <input
+                type="text"
+                placeholder="zipcode"
+                className="input-style w-full"
+              />
+            </div>
+            <button
+              data-aos="fade-up"
+              data-aos-delay="700"
+              className="primary-btn"
+            >
+              Search
+            </button>
+          </div>
+          {/* Map section  */}
+          <div data-aos="fade" className="col-span-2">
+            <img
+              src={worldmap}
+              alt=""
+              className="w-full sm:w-[500px] mx-auto"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default WhereToBuy;
