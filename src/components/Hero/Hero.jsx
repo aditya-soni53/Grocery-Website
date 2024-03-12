81972a1f5c156b20b437744e986cb27f8dac8895
aditya <adityasoni53@gmail.com>
2024-03-12 18:48:56 +0530

    sevice section completed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,17 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 import OrangeImg from "../../assets/orangebg.png";
+import {FaFacebookF , FaLinkedinIn , FaInstagram} from "react-icons/fa"
 
 const Hero = () => {
+
+  const [sidebar , setSidetbar] = React.useState(false) 
   return (
     <main className="md:px-12 md:py-6 bg-primaryDark">
       <section className="relative min-h-[550px] bg-gradient-to-r from-primary to-secondary w-full md:rounded-xl shadow-md">
         <div className="container">
           {/* Navbar */}
-
+          <Navbar sidebar={sidebar} setSidebar={setSidetbar}/>
           {/* Hero section */}
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center min-h-[550px] ">
             {/* Text content section */}
@@ -38,6 +42,29 @@ const Hero = () => {
         <h1 className="large-text">Orange</h1>
 
         {/* Sidebar section */}
+        {
+          sidebar && (
+            <div className="absolute top-0 right-0 w-[120px] h-full bg-gradient-to-b from-primary to-secondary z-10">
+              <div className="w-full h-full flex justify-center items-center">
+                <div className="text-white flex flex-col justify-center items-center gap-6">
+                  {/* line */}
+                  <div className="w-[1px] h-[70px] bg-white"></div>
+                  <div className="inline-block p-2 rounded-full cursor-pointer border border-white">
+                    <FaFacebookF className="text-2xl"/>
+                  </div>
+                  <div className="inline-block p-2 rounded-full cursor-pointer border border-white">
+                    <FaInstagram className="text-2xl"/>
+                  </div>
+                  <div className="inline-block p-2 rounded-full cursor-pointer border border-white">
+                    <FaLinkedinIn className="text-2xl"/>
+                  </div>
+                  {/* line */}
+                  <div className="w-[1px] h-[70px] bg-white"></div>
+                </div>
+              </div>
+            </div>
+          )
+        }
       </section>
     </main>
   );
